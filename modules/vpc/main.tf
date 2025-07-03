4d1a018c180b54d0d66358a941435b1e81893db4
Tayyab Sohail <tayyeb.suhail@gmail.com>
2025-07-03 15:02:17 +0200

    first commit module vpc

---
+++ modules/vpc/main.tf
@@ -0,0 +1,35 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "eks-vpc"
+    }
+  )
+}
+
+
+resource "aws_subnet" "public" {
+  for_each = {
+    for idx, cidr in var.public_subnet_cidrs :
+    idx => {
+      cidr = cidr
+      az   = var.azs[idx]
+    }
+  }
+
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = each.value.cidr
+  availability_zone       = each.value.az
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "public-subnet-${each.key}"
+    }
+  )
+}
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,43 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of availability zones to use"
+  type        = list(string)
+}
+
+variable "enable_nat_gateway" {
+  description = "Whether to create a NAT Gateway"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_support" {
+  description = "Enable DNS support in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_hostnames" {
+  description = "Enable DNS hostnames in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
