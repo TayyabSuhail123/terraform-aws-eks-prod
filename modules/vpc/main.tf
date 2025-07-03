e93885febc586101c6c326ccf539f9cc8c81fcf0
Tayyab Sohail <tayyeb.suhail@gmail.com>
2025-07-03 15:50:09 +0200

    fix

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -117,7 +117,7 @@ resource "aws_route_table" "private" {
   count = var.enable_nat_gateway ? 1 : 0
 
   vpc_id = aws_vpc.main_vpc.id
-
+  
   route {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat[0].id
